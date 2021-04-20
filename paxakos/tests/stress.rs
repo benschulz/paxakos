ecc7d205916279258897a2db2d5283431a43a343
Ben Schulz <benshu@benshu.de>
2021-04-20 21:34:07 +0200

    Rewrite stress test to terminate reliably

--- paxakos/tests/stress.rs
+++ paxakos/tests/stress.rs
@@ -3,13 +3,17 @@
 mod calc_app;
 mod tracer;
 
+use std::task::Poll;
+
+use futures::channel::oneshot;
 use futures::future::FutureExt;
 use futures::stream::{FuturesUnordered, StreamExt};
 use rand::seq::SliceRandom;
 use uuid::Uuid;
 
 use paxakos::append::AppendArgs;
-use paxakos::deco::FillGapsBuilderExt;
+use paxakos::deco::{EnsureLeadershipBuilderExt, FillGapsBuilderExt};
+use paxakos::deco::{SendHeartbeatsBuilderExt, TrackLeadershipBuilderExt};
 use paxakos::prototyping::{DirectCommunicator, PrototypingNode, RetryIndefinitely};
 use paxakos::{Node, NodeBuilder, NodeInfo, Shutdown};
 
@@ -39,7 +43,16 @@ fn stress_test() {
 
     let mut join_handles = Vec::new();
 
+    let mut trrs = Vec::new();
+    let mut dss = Vec::new();
+
     for n in nodes.clone() {
+        let (trs, trr) = oneshot::channel();
+        let (ds, dr) = oneshot::channel();
+
+        trrs.push(trr);
+        dss.push(ds);
+
         join_handles.push(spawn_node(
             n,
             nodes.clone(),
@@ -47,7 +60,17 @@ fn stress_test() {
             (1..=ops_per_node).map(|i| CalcOp::Add(f64::from(i), Uuid::new_v4())),
             target,
             &mut consistency_checker,
+            trs,
+            dr,
         ));
+    }
+
+    for trr in trrs {
+        futures::executor::block_on(trr).unwrap();
+    }
+
+    for ds in dss {
+        ds.send(()).unwrap();
     }
 
     consistency_checker.spawn();
@@ -66,14 +89,18 @@ fn stress_test() {
 fn spawn_node(
     node_info: PrototypingNode,
     all_nodes: Vec<PrototypingNode>,
-    mut communicator: CalcCommunicator,
+    communicator: CalcCommunicator,
     ops: impl std::iter::IntoIterator<Item = CalcOp> + Send + 'static,
     target: f64,
     consistency_checker: &mut StabilityChecker<usize, u64, u32, Uuid>,
+    target_reached_sender: oneshot::Sender<()>,
+    mut done_receiver: oneshot::Receiver<()>,
 ) -> std::thread::JoinHandle<blake3::Hash> {
     let tracer = consistency_checker.tracer(node_info.id());
 
     std::thread::spawn(move || {
+        let node_id = node_info.id();
+
         let (handler, node) = futures::executor::block_on(
             paxakos::node_builder()
                 .for_node(node_info.id())
@@ -85,6 +112,17 @@ fn spawn_node(
                     c.with_entry(|| CalcOp::Sub(0.0, Uuid::new_v4()))
                         .after(std::time::Duration::from_millis(50))
                         .retry_every(std::time::Duration::from_millis(20))
+                })
+                .send_heartbeats(|c| {
+                    tracing::info!("Node {:?} is sending a heartbeat.", node_id);
+
+                    c.with_entry(|| CalcOp::Mul(1.0, Uuid::new_v4()))
+                        .every(std::time::Duration::from_millis(200))
+                })
+                .track_leadership()
+                .ensure_leadership(|c| {
+                    c.with_entry(|| CalcOp::Div(1.0, Uuid::new_v4()))
+                        .every(std::time::Duration::from_millis(500))
                 })
                 .spawn_in(()),
         )
@@ -98,79 +136,68 @@ fn spawn_node(
         let mut running_ops = FuturesUnordered::new();
         let mut hash_at_target = None;
 
-        let mut deadline = None;
+        let mut target_reached_sender = Some(target_reached_sender);
+
+        let handle = node.handle();
+
+        std::thread::spawn(move || {
+            futures::executor::block_on(async move {
+                loop {
+                    while !queued_ops.is_empty() && running_ops.len() < 10 {
+                        let op = queued_ops.pop().unwrap();
+
+                        running_ops.push(
+                            handle
+                                .append(
+                                    op,
+                                    AppendArgs {
+                                        retry_policy: Box::new(RetryIndefinitely::pausing_up_to(
+                                            std::time::Duration::from_millis(200),
+                                        )),
+                                        ..Default::default()
+                                    },
+                                )
+                                .map(move |r| r.err().map(|_| op)),
+                        );
+
+                        tracing::debug!("Remaining ops in queue: {}", queued_ops.len());
+                    }
+
+                    if let Some(res) = running_ops.next().await {
+                        if let Some(op) = res {
+                            queued_ops.push(op);
+                        } else if queued_ops.is_empty() && running_ops.is_empty() {
+                            tracing::info!("Node {:?} is out of work.", node_info.id());
+                            return;
+                        }
+                    }
+                }
+            })
+        });
 
         let mut node = Some(node);
 
-        let deadline_duration = std::time::Duration::from_millis(750);
-
         let node = futures::executor::block_on(futures::future::poll_fn(|cx| {
-            while let std::task::Poll::Ready(e) = node.as_mut().unwrap().poll_events(cx) {
-                match (&mut deadline, e) {
-                    (deadline @ None, paxakos::Event::Apply { result, .. }) => {
-                        if (target - result.0).abs() < f64::EPSILON {
-                            tracing::info!(
-                                "Node {} reached target, sets deadline.",
-                                node_info.id()
-                            );
+            if done_receiver.poll_unpin(cx).is_ready() {
+                return Poll::Ready(node.take().unwrap());
+            }
+
+            while let Poll::Ready(e) = node.as_mut().unwrap().poll_events(cx) {
+                if let paxakos::Event::Apply { result, .. } = e {
+                    if (target - result.0).abs() < f64::EPSILON {
+                        if let Some(target_reached_sender) = target_reached_sender.take() {
+                            tracing::info!("Node {} reached target.", node_info.id());
 
                             hash_at_target = Some(result.1);
-                            *deadline = Some(futures_timer::Delay::new(deadline_duration));
-
-                            // make sure that everyone gets a chance to finish
-                            communicator.failure_rate(0.0);
-                            queued_ops.push(CalcOp::Div(1.0, Uuid::new_v4()));
+                            target_reached_sender.send(()).unwrap();
                         }
                     }
-                    (deadline @ Some(_), _) => {
-                        tracing::info!("Node {} extends deadline.", node_info.id());
-                        // extend the deadline
-                        *deadline = Some(futures_timer::Delay::new(deadline_duration));
-                    }
-                    _ => {}
                 }
             }
 
-            if let Some(deadline) = deadline.as_mut() {
-                if let std::task::Poll::Ready(_) = deadline.poll_unpin(cx) {
-                    return std::task::Poll::Ready(node.take());
-                }
-            }
-
-            while !queued_ops.is_empty() && running_ops.len() < 10 {
-                let op = queued_ops.pop().unwrap();
-
-                running_ops.push(
-                    node.as_mut()
-                        .unwrap()
-                        .append(
-                            op,
-                            AppendArgs {
-                                retry_policy: Box::new(RetryIndefinitely::pausing_up_to(
-                                    std::time::Duration::from_millis(200),
-                                )),
-                                ..Default::default()
-                            },
-                        )
-                        .map(move |r| r.err().map(|_| op))
-                        .boxed_local(),
-                );
-
-                tracing::debug!("Remaining ops in queue: {}", queued_ops.len());
-            }
-
-            while let std::task::Poll::Ready(Some(res)) = running_ops.poll_next_unpin(cx) {
-                if let Some(op) = res {
-                    queued_ops.push(op);
-                } else if queued_ops.is_empty() && running_ops.is_empty() {
-                    tracing::info!("Node {:?} is out of work.", node_info.id());
-                }
-            }
-
-            std::task::Poll::Pending
+            Poll::Pending
         }));
 
-        let node = node.unwrap();
         let mut shutdown = node.shut_down();
 
         let snapshot = futures::executor::block_on(
