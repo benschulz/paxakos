01eb0cd5f956902cfc3a1fa86e0e37e605ca0131
Ben Schulz <benshu@benshu.de>
2021-04-20 21:34:07 +0200

    Remove a TODO marker and add two new ones

--- paxakos/src/node/info.rs
+++ paxakos/src/node/info.rs
@@ -5,12 +5,23 @@ use crate::Identifier;
 /// Beyond an identifier, a `NodeInfo` implementation provides all the
 /// information necessary to communicate with that node, typically a hostname
 /// and a (TCP) port.
-// TODO not wild about the name…
 pub trait NodeInfo: 'static + Clone + std::fmt::Debug + Send + Sync {
     type Id: Identifier;
 
     /// A unique identifier for this node.
     fn id(&self) -> Self::Id;
+
+    // TODO
+    // type Weight: num_traits::One = u32;
+    // fn weight(&self) -> Self::Weight {
+    //     num_traits::One::one()
+    // }
+
+    // TODO
+    // type Priority: Default = i32;
+    // fn priority(&self) -> Self::Priority {
+    //     Default::default()
+    // }
 }
 
 /// To toy around with a single node cluster.
