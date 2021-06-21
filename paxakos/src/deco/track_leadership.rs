a4b08d259a76a16de957100958924bab760158f2
Ben Schulz <benshu@benshu.de>
2021-06-22 00:04:41 +0200

    Refactor leadership tracking slightly

--- paxakos/src/deco/track_leadership.rs
+++ paxakos/src/deco/track_leadership.rs
@@ -3,14 +3,14 @@ use std::ops::RangeInclusive;
 use std::task::Poll;
 
 use futures::future::LocalBoxFuture;
-use num_traits::{Bounded, One};
+use num_traits::{Bounded, One, Zero};
 
 use crate::append::AppendArgs;
 use crate::applicable::ApplicableTo;
 use crate::communicator::Communicator;
 use crate::error::Disoriented;
 use crate::node::builder::NodeBuilder;
-use crate::node::{AbstentionOf, AppendResultFor, CommunicatorOf, CoordNumOf, RejectionOf};
+use crate::node::{AbstentionOf, AppendResultFor, CommunicatorOf, CoordNumOf, NodeOf, RejectionOf};
 use crate::node::{Node, NodeIdOf, NodeInfo, NodeKernel, NodeStatus, Participation};
 use crate::node::{RoundNumOf, Snapshot, SnapshotFor, StateOf};
 use crate::state::State;
@@ -27,7 +27,7 @@ pub trait LeadershipAwareNode<I>: Node {
     /// Note: This information is unreliable. A node will assume itself or
     /// another node to be leader when that node "owns" the highest coordination
     /// number observed for that round. That means neither that the node managed
-    /// to achieve a quorum with that number, nor that no other node hasn't
+    /// to achieve a quorum with that number, nor that no other node
     /// achieved a quorum with an even higher number.
     // TODO consider renaming to "lax_leadership" and introducing a strict version
     //      which disregards prepare messages
@@ -67,6 +67,7 @@ where
 pub struct TrackLeadership<N: Node> {
     decorated: N,
     suspended: bool,
+    min_round: RoundNumOf<N>,
     mandates: BTreeMap<RoundNumOf<N>, Mandate<N>>,
     leadership: Vec<LeadershipFor<N>>,
 }
@@ -84,6 +85,95 @@ pub struct Leadership<N, R: RoundNum, C> {
     pub last_directive_at: std::time::Instant,
 }
 
+impl<N: Node> TrackLeadership<N> {
+    fn react_to_apply(&mut self, round: RoundNumOf<N>) {
+        let affects_first_mandate = self
+            .mandates
+            .first_key_value()
+            .filter(|(r, _)| **r <= round)
+            .is_some();
+
+        if affects_first_mandate {
+            let next_round = round + One::one();
+            let mandate = self.mandates.remove(&round).unwrap();
+
+            let obsolete = self
+                .mandates
+                .first_key_value()
+                .filter(|(r, _)| **r == next_round)
+                .is_some();
+
+            if obsolete {
+                self.leadership.remove(0);
+            } else {
+                self.mandates.insert(next_round, mandate);
+                self.leadership[0].rounds = next_round..=*self.leadership[0].rounds.end();
+            }
+        }
+    }
+
+    fn react_to_directive(
+        &mut self,
+        leader: &NodeOf<N>,
+        round_num: RoundNumOf<N>,
+        coord_num: CoordNumOf<N>,
+    ) {
+        let leader = leader.id();
+
+        let now = std::time::Instant::now();
+
+        let known_mandate = self
+            .mandates
+            .range_mut(..=round_num)
+            .last()
+            .filter(|(_, m)| m.mandate >= coord_num);
+
+        if let Some((_, m)) = known_mandate {
+            if m.mandate == coord_num {
+                m.last_directive_at = now;
+
+                for l in &mut self.leadership {
+                    if l.mandate == coord_num {
+                        l.last_directive_at = now;
+                    }
+                }
+            }
+        } else {
+            self.mandates
+                .drain_filter(|r, m| *r >= round_num && coord_num >= m.mandate);
+
+            self.mandates.insert(
+                round_num,
+                Mandate {
+                    mandate: coord_num,
+                    leader,
+                    last_directive_at: now,
+                },
+            );
+
+            let mandates = self.mandates.iter().collect::<Vec<_>>();
+
+            self.leadership.clear();
+            for [(r1, m), (r2, _)] in mandates.array_windows::<2>() {
+                self.leadership.push(Leadership {
+                    leader: m.leader,
+                    rounds: **r1..=(**r2 - One::one()),
+                    mandate: m.mandate,
+                    last_directive_at: m.last_directive_at,
+                });
+            }
+
+            let (r, m) = mandates.last().unwrap();
+            self.leadership.push(Leadership {
+                leader: m.leader,
+                rounds: **r..=Bounded::max_value(),
+                mandate: m.mandate,
+                last_directive_at: m.last_directive_at,
+            });
+        }
+    }
+}
+
 impl<N> Decoration for TrackLeadership<N>
 where
     N: Node + 'static,
@@ -99,6 +189,7 @@ where
         Ok(Self {
             decorated,
             suspended: false,
+            min_round: Zero::zero(),
             mandates: BTreeMap::new(),
             leadership: Vec::new(),
         })
@@ -140,14 +231,14 @@ where
         let event = self.decorated.poll_events(cx);
 
         if let Poll::Ready(event) = &event {
-            if let crate::Event::Init { .. } | crate::Event::Install { .. } = event {
-                self.suspended = true;
-                self.mandates.clear();
-                self.leadership.clear();
-            }
-
-            if let crate::Event::StatusChange { new_status, .. } = event {
-                match new_status {
+            match event {
+                crate::Event::Init { round, .. } | crate::Event::Install { round, .. } => {
+                    self.suspended = true;
+                    self.min_round = *round + One::one();
+                    self.mandates.clear();
+                    self.leadership.clear();
+                }
+                crate::Event::StatusChange { new_status, .. } => match new_status {
                     NodeStatus::Lagging | NodeStatus::Disoriented => {
                         if !self.suspended {
                             self.suspended = true;
@@ -156,108 +247,24 @@ where
                         }
                     }
                     NodeStatus::Following | NodeStatus::Leading => self.suspended = false,
-                }
-            }
+                },
+                crate::Event::Apply { round, .. } => {
+                    self.min_round = *round + One::one();
 
-            if !self.suspended {
-                if let crate::Event::Directive {
+                    if !self.suspended {
+                        self.react_to_apply(*round);
+                    }
+                }
+                crate::Event::Directive {
                     leader,
                     round_num,
                     coord_num,
-                } = event
-                {
-                    let leader = leader.id();
-                    let round_num = *round_num;
-                    let coord_num = *coord_num;
-
-                    let now = std::time::Instant::now();
-
-                    let known_mandate = self
-                        .mandates
-                        .range_mut(..=round_num)
-                        .last()
-                        .filter(|(_, m)| m.mandate >= coord_num);
-
-                    if let Some((_, m)) = known_mandate {
-                        if m.mandate == coord_num {
-                            m.last_directive_at = now;
-
-                            for l in &mut self.leadership {
-                                if l.mandate == coord_num {
-                                    l.last_directive_at = now;
-                                }
-                            }
-                        }
-                    } else {
-                        self.mandates
-                            .drain_filter(|r, m| *r >= round_num && coord_num >= m.mandate);
-
-                        self.mandates.insert(
-                            round_num,
-                            Mandate {
-                                mandate: coord_num,
-                                leader,
-                                last_directive_at: now,
-                            },
-                        );
-
-                        let mandates = self.mandates.iter().collect::<Vec<_>>();
-
-                        self.leadership.clear();
-                        for [(r1, m), (r2, _)] in mandates.array_windows::<2>() {
-                            self.leadership.push(Leadership {
-                                leader: m.leader,
-                                rounds: **r1..=(**r2 - One::one()),
-                                mandate: m.mandate,
-                                last_directive_at: m.last_directive_at,
-                            });
-                        }
-
-                        let (r, m) = mandates.last().unwrap();
-                        self.leadership.push(Leadership {
-                            leader: m.leader,
-                            rounds: **r..=Bounded::max_value(),
-                            mandate: m.mandate,
-                            last_directive_at: m.last_directive_at,
-                        });
+                } => {
+                    if !self.suspended && *round_num >= self.min_round {
+                        self.react_to_directive(leader, *round_num, *coord_num);
                     }
                 }
-
-                if let crate::Event::Apply { round, .. } = event {
-                    let round = *round;
-
-                    let affects_first_mandate = self
-                        .mandates
-                        .first_key_value()
-                        .filter(|(r, _)| {
-                            if **r <= round {
-                                assert_eq!(**r, round);
-                                true
-                            } else {
-                                false
-                            }
-                        })
-                        .is_some();
-
-                    if affects_first_mandate {
-                        let next_round = round + One::one();
-                        let mandate = self.mandates.remove(&round).unwrap();
-
-                        let obsolete = self
-                            .mandates
-                            .first_key_value()
-                            .filter(|(r, _)| **r == next_round)
-                            .is_some();
-
-                        if obsolete {
-                            self.leadership.remove(0);
-                        } else {
-                            self.mandates.insert(next_round, mandate);
-                            self.leadership[0].rounds =
-                                next_round..=*self.leadership[0].rounds.end();
-                        }
-                    }
-                }
+                _ => {}
             }
         }
 
