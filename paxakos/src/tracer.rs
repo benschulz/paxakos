86c034ccdfe0487cc013ab2f310267fde51b93fb
Ben Schulz <benshu@benshu.de>
2021-10-02 22:54:35 +0200

    Document tracer module

--- paxakos/src/tracer.rs
+++ paxakos/src/tracer.rs
@@ -1,9 +1,19 @@
+//! Defines the [`Tracer`] trait.
+
 use crate::invocation::CoordNumOf;
 use crate::invocation::Invocation;
 use crate::invocation::LogEntryIdOf;
 use crate::invocation::RoundNumOf;
 
+/// Records events as they are observed by a node.
+///
+/// Tracing events isn't useful under normal operation. It can aid in
+/// understanding when and how inconsistencies were introduced. As such, some
+/// tests set a tracer so that they can emit a trace if they fail.
+///
+/// See [`NodeBuilder::traced_by`][crate::NodeBuilder::traced_by].
 pub trait Tracer<I: Invocation>: Send {
+    /// A promise was made.
     fn record_promise(
         &mut self,
         round_num: RoundNumOf<I>,
@@ -11,6 +21,7 @@ pub trait Tracer<I: Invocation>: Send {
         promise: Vec<(RoundNumOf<I>, CoordNumOf<I>, LogEntryIdOf<I>)>,
     );
 
+    /// A proposal was accepted.
     fn record_accept(
         &mut self,
         round_num: RoundNumOf<I>,
@@ -18,5 +29,6 @@ pub trait Tracer<I: Invocation>: Send {
         log_entry_id: LogEntryIdOf<I>,
     );
 
+    /// A log entry was committed.
     fn record_commit(&mut self, round_num: RoundNumOf<I>, log_entry_id: LogEntryIdOf<I>);
 }
